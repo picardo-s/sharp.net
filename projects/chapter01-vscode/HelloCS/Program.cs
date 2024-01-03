182ad0f7b92cef2979db9d5a67743fd5f3c4fef2
Roman Romanteev <Roman.ROMANTEEV.ext@hitachirail.com>
2024-01-03 17:41:46 +0300

    Working on chapter 2

--- projects/chapter01-vscode/HelloCS/Program.cs
+++ projects/chapter01-vscode/HelloCS/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
+int z;
 throw new Exception();
---
+++ projects/chapter02-vscode/vocabulary/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
