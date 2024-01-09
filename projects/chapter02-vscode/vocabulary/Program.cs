4c81625226faa2e29b663058c0bbc67ef66d7168
Roman Romanteev <Roman.ROMANTEEV.ext@hitachirail.com>
2024-01-09 18:17:25 +0300

    Done chapter 3

--- projects/chapter02-vscode/vocabulary/Program.cs
+++ projects/chapter02-vscode/vocabulary/Program.cs
@@ -1,9 +1,11 @@
-﻿Console.WriteLine("Press any key combination: ");
+﻿string horizontalLine = new string('-', 113);
 
-ConsoleKeyInfo key;
-do
-{
-    key = ReadKey();
-    Console.WriteLine();
-    Console.WriteLine("Key: {0}, Char: {1}, Modifiers: {2}", key.Key, key.KeyChar, key.Modifiers);
-}while(key.KeyChar != 'X');
+Console.WriteLine(horizontalLine);
+Console.WriteLine("{0,-10} {1, -20} {2, 30} {3, 50}", "Type", "Byte(s) of memory", "Min", "Max");
+Console.WriteLine(horizontalLine);
+
+Console.WriteLine($"{"sbyte",-10} {sizeof(sbyte), -2} {sbyte.MinValue, 48} {sbyte.MaxValue, 50}");
+Console.WriteLine($"{"byte",-10} {sizeof(byte), -2} {byte.MinValue, 48} {byte.MaxValue, 50}");
+Console.WriteLine($"{"short",-10} {sizeof(short), -2} {short.MinValue, 48} {short.MaxValue, 50}");
+Console.WriteLine($"{"decimal",-10} {sizeof(decimal), -2} {decimal.MinValue, 48} {decimal.MaxValue, 50}");
+Console.WriteLine(horizontalLine);
