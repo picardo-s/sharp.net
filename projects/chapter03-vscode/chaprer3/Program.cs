619d7a2644ed4111bf27fc24fa321f89696f8a98
Roman Romanteev <Roman.ROMANTEEV.ext@hitachirail.com>
2024-01-11 20:15:11 +0300

    working on chapter 3

---
+++ projects/chapter03-vscode/chaprer3/Program.cs
@@ -0,0 +1,15 @@
+﻿string[,] grid1 = new[,]
+{
+    { "Alpha", "Beta", "Gamma", "Delta" },
+    { "Anne", "Ben", "Charlie", "Doug" },
+    { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+WriteLine("{0} {1} {2} {3}",
+    grid1.GetLowerBound(0),
+    grid1.GetUpperBound(0),
+    grid1.GetLowerBound(1),
+    grid1.GetUpperBound(1)
+);
+
+WriteLine(grid1[0,0]);
