2276ef0bc3ce42641a4fdc6423917711ae33b641
Roman Romanteev <Roman.ROMANTEEV.ext@hitachirail.com>
2024-03-13 17:43:06 +0300

    Updated gitignore file. Working on chapter3

---
+++ projects/Chapter3_AllProject/SimpleCSharpApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//Display a simple message to the user
+Console.WriteLine("***** My First (no) C# App *****");
+Console.WriteLine();
+
+Console.ReadLine();
+
+string[] theArgs = Environment.GetCommandLineArgs();
+
+foreach(string arg in theArgs)
+{
+    Console.WriteLine($"Arg: {arg}");
+}
+
+return -1;
---
+++ projects/Chapter3_AllProject/SimpleCSharpApp/SimpleCSharpApp.cmd
@@ -0,0 +1,17 @@
+@echo off
+rem A batch file for SimpleCSharpApp.exe
+rem which captures the app's return value.
+
+dotnet run /arg1 -arg2
+@if "%ERRORLEVEL%" == "0" goto success
+
+:fail
+    echo This application has failed
+    echo return value = %ERRORLEVEL%
+    goto end
+:success
+    echo This application has succeeded!
+    echo return value = %ERRORLEVEL%
+    goto end
+:end
+echo All Done.
